c9a6f3320b87ac075b355e46e9b1e80316dab30f
Felipe Azevedo <felipe.azevedo@gmail.com>
2026-05-29 13:49:20 -0300

    Configura health checks de liveness e readiness

--- src/CreditoFiscal.Api/Program.cs
+++ src/CreditoFiscal.Api/Program.cs
@@ -4,7 +4,10 @@ using CreditoFiscal.Infraestrutura.Json;
 using CreditoFiscal.Infraestrutura.Mensageria;
 using CreditoFiscal.Infraestrutura.Persistencia;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,11 +20,19 @@ builder.Services.AdicionarPersistencia(builder.Configuration);
 builder.Services.AdicionarMensageria(builder.Configuration);
 builder.Services.AddHostedService<CreditoConsumer>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Postgres")!, name: "postgres", tags: new[] { "ready" })
+    .AddRabbitMQ(sp => sp.GetRequiredService<IConnection>(), name: "rabbitmq", tags: new[] { "ready" });
+
 var app = builder.Build();
 
 // primeiro do pipeline: captura excecao de tudo que vem abaixo
 app.UseMiddleware<ExcecoesMiddleware>();
 
 app.MapControllers();
+
+// /self = liveness (processo de pe); /ready = readiness (Postgres e RabbitMQ alcancaveis)
+app.MapHealthChecks("/self", new HealthCheckOptions { Predicate = _ => false });
+app.MapHealthChecks("/ready", new HealthCheckOptions { Predicate = registro => registro.Tags.Contains("ready") });
 
 app.Run();
